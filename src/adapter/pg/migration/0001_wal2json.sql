c5dfe24904ef81e2e11614bd30eb648c9b5825f4
suehyun73 <101646633+suehyun73@users.noreply.github.com>
2025-01-30 00:39:33 +0900

    chore: 볼륨 추가

--- src/adapter/pg/migration/0001_wal2json.sql
+++
@@ -1,9 +0,0 @@
-
--- logical replication을 위해 필요
-ALTER SYSTEM SET wal_level = 'logical';
-
--- wal을 읽어들일 수 있는 외부 시스템 최대 개수
-ALTER SYSTEM SET max_replication_slots = 4;
-
--- wal process 개수
-ALTER SYSTEM SET max_wal_senders = 4;
